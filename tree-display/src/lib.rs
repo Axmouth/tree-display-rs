bd752320cecc4a7ed5787224348b82ff8f55c201
Axmouth <aggronfg@yahoo.com>
2022-02-19 02:36:11 +0200

    Initial support for arrays/vectors

--- tree-display/src/lib.rs
+++ tree-display/src/lib.rs
@@ -54,6 +54,59 @@ where
     }
 }
 
+
+impl<T> TreeDisplay for [T]
+where
+    T: TreeDisplay,
+{
+    fn tree_fmt<'a>(
+        &'a self,
+        f: &mut std::fmt::Formatter<'_>,
+        indent: &str,
+        show_types: bool,
+    ) -> std::fmt::Result {
+        if show_types {
+            writeln!(f, "({})", "Vec")?;
+        }
+        let mut new_indent = indent.to_string();
+        new_indent.push_str("|  ");
+        for (i, item) in self.iter().enumerate() {
+            if i < self.len() - 1 {
+                write!(f, "{}├─", indent)?;
+            } else {
+                write!(f, "{}└─", indent)?;
+            }
+            item.tree_fmt(f, &format!("{}  ", indent), show_types)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T> TreeDisplay for Vec<T>
+where
+    T: TreeDisplay,
+{
+    fn tree_fmt<'a>(
+        &'a self,
+        f: &mut std::fmt::Formatter<'_>,
+        indent: &str,
+        show_types: bool,
+    ) -> std::fmt::Result {
+        self[..].tree_fmt(f, indent, show_types)
+    }
+}
+
+impl TreeDisplay for () {
+    fn tree_fmt<'a>(
+        &'a self,
+        f: &mut std::fmt::Formatter<'_>,
+        indent: &str,
+        _: bool,
+    ) -> std::fmt::Result {
+        writeln!(f, "({})\n{}└─", stringify!(()), indent)
+    }
+}
+
 tree_display_impl_primitive!(
     i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, bool, char, &str,
     String
